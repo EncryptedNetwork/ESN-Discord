a848bea58c9ab953efff64c1d23e3021b924f1b7
Reality <rchahine1@gmail.com>
2017-09-21 20:38:08 -0400

    Consistent uptime update

--- index.js
+++ index.js
@@ -9,6 +9,10 @@ require('events').EventEmitter.defaultMaxListeners = Infinity
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+app.get('/wakemydyno.txt', function (req, res) {
+    res.render('pages/wakemydyno')
+})
+
 app.listen(app.get('port'), function () {
     console.log('Website Service Online (' + app.get('port') + ").")
 })
