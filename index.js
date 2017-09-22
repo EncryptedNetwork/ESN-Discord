4a886546c9bfb4ecdd22aa39174b036b233f0838
Reality <rchahine1@gmail.com>
2017-09-21 20:41:20 -0400

    Consistent uptime 2.0

--- index.js
+++ index.js
@@ -8,6 +8,8 @@ require('events').EventEmitter.defaultMaxListeners = Infinity
 
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
+app.set('views', __dirname + '/views')
+app.set('view engine', 'ejs')
 
 app.get('/wakemydyno.txt', function (req, res) {
     res.render('pages/wakemydyno')
